eb28a859a2ea6a0e804ad2aecba24aef54f0be9c
aliyamhaiskar19 <maheskaraliya3681@gmail.com>
2026-05-31 19:51:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+window.addEventListener("scroll", function(){
+
+    const navbar = document.querySelector(".navbar");
+
+    navbar.classList.toggle("scrolled", window.scrollY > 50);
+
+});
